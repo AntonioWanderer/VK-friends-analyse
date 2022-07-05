4af6ddd8e6db4bc15f7d94e2fccc8a2c7c3f7297
AntonioWanderer <areikow@mail.ru>
2022-07-05 22:36:56 +0700

    zero-approximation works

--- Statistics.py
+++ Statistics.py
@@ -3,6 +3,10 @@ import sqlite3
 import matplotlib.pyplot as plt
 import numpy as np
 
+IDGLOB = [444883822,
+	208358821,
+	217241399]
+
 def sqlInit(i):
 	conn = sqlite3.connect("requests/requests"+str(i)+".db",
                        detect_types=sqlite3.PARSE_DECLTYPES |
@@ -20,6 +24,20 @@ def sqlClose(conn, cur):
 	cur.close()
 	conn.close()
 	
+def idList():
+	ids = []
+	for i in range(len(os.listdir("/home/antonio/OnlineRequests simp/requests"))):
+		conn, cur = sqlInit(i)
+		formula = "SELECT * FROM online_requests"
+		cur.execute(formula)
+		all_results = cur.fetchall()
+		sqlClose(conn, cur)
+		for result in all_results:
+			thisId = result[2]
+			if not thisId in ids:
+				ids.append(thisId)
+	return(sorted(ids))
+	
 def resultsLoad(i, uid):
 	conn, cur = sqlInit(i)
 	formula = "SELECT * FROM online_requests WHERE usr_online == " + str(uid)
@@ -30,7 +48,6 @@ def resultsLoad(i, uid):
 	
 def fullBase(uid):
 	base = []
-	x = []
 	for i in range(len(os.listdir("/home/antonio/OnlineRequests simp/requests"))):
 		for result in resultsLoad(i + 1, uid):
 			base.append(result)
@@ -49,11 +66,31 @@ def comparator(base1, base2):
 		if t in t2:
 			common = common + 1
 	full = len(t1) + len(t2) - common
-	return(common / full)
+	return(common)
 
 def compCouple(id1, id2):
-	base1 = fullBase(id1)#650125)
-	base2 = fullBase(id2)#111402557)
-return comparator(base1, base2)
+	base1 = fullBase(id1)
+	base2 = fullBase(id2)
+	return comparator(base1, base2)
 
-
+def compAndDescribeAll():
+	ids = IDGLOB
+	leng = len(ids)
+	matrix = np.zeros((leng, leng))
+	flatten = []
+	#ids = idList()
+	i = 0
+	for i1 in range(leng):
+		for i2 in range(i1 + 1):
+			z = compCouple(ids[i1], ids[i2])/6
+			print(ids[i1], ids[i2])
+			matrix[i1][i2] = z
+			matrix[i2][i1] = z
+			if z != 1:
+				flatten.append(z)
+			i = i + 1
+			#print("progress ", int(100 * i * 2 / ((leng) * (leng + 1))), "%")
+	return(matrix, flatten)
+	
+matrix, flatten = compAndDescribeAll()
+print(matrix)
